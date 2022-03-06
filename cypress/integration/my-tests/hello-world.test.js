c52510b76dd103bb2923e8ff9cd9c60b61a5225c
Sashankr <sashankr97@gmail.com>
2022-03-06 19:46:43 +0530

    cypress should

--- cypress/integration/my-tests/hello-world.test.js
+++ cypress/integration/my-tests/hello-world.test.js
@@ -6,5 +6,6 @@ describe('Empty test',()=>{
         cy.contains('Build projects, practice and learn to code from scratch - without leaving your browser.')
         cy.get('header.cd-morph-dropdown').should('exist');
         cy.get('a').contains('Explore All Roadmaps').should('exist').click();
+        cy.get('h1.text-center').should('have.text','Learning Paths');
     })
 })
