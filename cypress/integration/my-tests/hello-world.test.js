dcdd565cca9397390fcbe512f1736c868b86c333
Sashankr <sashankr97@gmail.com>
2022-03-06 19:25:35 +0530

    cy get method

--- cypress/integration/my-tests/hello-world.test.js
+++ cypress/integration/my-tests/hello-world.test.js
@@ -4,5 +4,7 @@ describe('Empty test',()=>{
     it('test one', () =>{
         cy.visit('https://codedamn.com/')
         cy.contains('Build projects, practice and learn to code from scratch - without leaving your browser.')
+        cy.get('header.cd-morph-dropdown').should('exist');
+        cy.get('a').contains('Explore All Roadmaps').should('exist').click();
     })
 })
