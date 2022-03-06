b8752d92240c8d6ded460af73a8f57cee874988f
Sashankr <sashankr97@gmail.com>
2022-03-06 19:54:05 +0530

    cy.viewport

--- cypress/integration/my-tests/hello-world.test.js
+++ cypress/integration/my-tests/hello-world.test.js
@@ -1,7 +1,17 @@
 /// <reference types="cypress" />
 
 describe('Empty test',()=>{
-    it('test one', () =>{
+    it('test desktop', () =>{
+        cy.viewport("macbook-13")
+        cy.visit('https://codedamn.com/')
+        cy.contains('Build projects, practice and learn to code from scratch - without leaving your browser.')
+        cy.get('header.cd-morph-dropdown').should('exist');
+        cy.get('a').contains('Explore All Roadmaps').should('exist').click();
+        cy.get('h1.text-center').should('have.text','Learning Paths');
+    })
+
+    it('test mobile', () =>{
+        cy.viewport("iphone-xr")
         cy.visit('https://codedamn.com/')
         cy.contains('Build projects, practice and learn to code from scratch - without leaving your browser.')
         cy.get('header.cd-morph-dropdown').should('exist');
