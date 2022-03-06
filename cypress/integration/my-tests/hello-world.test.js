3391aa917c410e5349193044e1153f40a2ddc97f
Sashankr <sashankr97@gmail.com>
2022-03-06 19:04:50 +0530

    cy contains

--- cypress/integration/my-tests/hello-world.test.js
+++ cypress/integration/my-tests/hello-world.test.js
@@ -2,6 +2,7 @@
 
 describe('Empty test',()=>{
     it('test one', () =>{
-        cy.visit('https://sashankr.com')
+        cy.visit('https://codedamn.com/')
+        cy.contains('Build projects, practice and learn to code from scratch - without leaving your browser.')
     })
 })
