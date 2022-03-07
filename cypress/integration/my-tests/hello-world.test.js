2e4eb206efe94d69b2e06afc562467f4c8f8dc6f
Sashankr <sashankr97@gmail.com>
2022-03-07 09:07:19 +0530

    testing for link with cy.url()

--- cypress/integration/my-tests/hello-world.test.js
+++ cypress/integration/my-tests/hello-world.test.js
@@ -9,10 +9,22 @@ describe('Empty test',()=>{
     it('Check sign in page', () =>{
         cy.viewport("macbook-13")
         cy.visit('https://codedamn.com/')
+        
+        // 1.Visit Sign in page
         cy.contains('Sign in').should('exist').click();
-        cy.contains('Register').should('exist');
-        cy.get('[data-testid=login]').should('exist');
-        cy.contains('Forgot your password?').should('exist');
-        cy.contains('Create one').should('exist');
+        cy.url().should('include','/login');
+        
+        // //2. Checking forgot password url
+        // cy.get('a').contains('Forgot your password?').should('exist').click();
+        // // cy.url().should('include','/password-reset');
+        // // cy.go('back');
+
+
+        //3. Checking register page url
+
+        cy.contains('Create one').should('exist').click();
+        cy.url().should('include','/register');
+        cy.go('back');
+
     })
 })
