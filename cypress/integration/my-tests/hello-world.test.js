9647a73073fd047f6bd0b7e57bb8a9c4e79753fe
Sashankr <sashankr97@gmail.com>
2022-03-09 11:34:20 +0530

    setting local storage with cypress

--- cypress/integration/my-tests/hello-world.test.js
+++ cypress/integration/my-tests/hello-world.test.js
@@ -1,9 +1,33 @@
 /// <reference types="cypress" />
 
-describe('Empty test',()=>{
-    it('Check if page loads', () =>{
+describe('Basic tests',()=>{
+
+
+    beforeEach(()=>{
         cy.viewport("macbook-13")
         cy.visit('https://codedamn.com/')
+        cy.then(()=>{
+            window.localStorage.setItem('login',true)
+        })
+    })
+
+
+    it('Check sign in page error', {scrollBehavior : false} ,  () =>{
+        cy.viewport("macbook-13")
+        cy.visit('https://codedamn.com/')
+        
+        // 1.Visit Sign in page
+        cy.contains('Sign in').should('exist').click();
+        cy.url().then(value => {
+            cy.log('current url',value)
+        })
+        cy.url().should('include','/login');
+        cy.contains('Unable to authorize').should('not.exist');
+
+        cy.get('[data-testid = username]').type('admin')
+        cy.get('[data-testid = password]').type('admin')
+        cy.get('[data-testid=login]').should('exist').click()
+        cy.contains('Unable to authorize').should('exist');
     })
 
     it.only('Check sign in page error', {scrollBehavior : false} ,  () =>{
